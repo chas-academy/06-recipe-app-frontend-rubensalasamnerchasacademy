90602c7e8fc1df8ea0090f91d923edd77cbea723
Ruben Salas Amner <ruben.salas.amner@chasacademy.se>
2019-02-15 12:54:12 +0100

    add routes and imports for all components in app-routing.module

--- recipeappfront/src/app/app-routing.module.ts
+++ recipeappfront/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { RecipesdetailComponent } from './recipesdetail/recipesdetail.component';
+import { RecipeslistComponent } from './recipeslist/recipeslist.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/recipeslist', pathMatch: 'full'},
+  { path : 'recipeslist', component: RecipeslistComponent},
+  { path : 'recipesdetail/:id', component: RecipesdetailComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
