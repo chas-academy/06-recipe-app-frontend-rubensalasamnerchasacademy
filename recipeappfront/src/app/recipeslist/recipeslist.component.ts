8e7a01cd31bd428ddb411844c9b5371db6ff0d5f
Ruben Salas Amner <ruben.salas.amner@chasacademy.se>
2019-02-20 08:49:20 +0100

    add functionality into recipeslist that calls on the method in recipesearch.service and makes an api request based on user input, also add a select to allow filtering returned recipes

--- recipeappfront/src/app/recipeslist/recipeslist.component.ts
+++ recipeappfront/src/app/recipeslist/recipeslist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RecipesearchService } from '../recipesearch.service';
 
 @Component({
   selector: 'app-recipeslist',
@@ -8,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeslistComponent implements OnInit {
 
-  constructor() { }
+  recipes: string [];
+  searchinput = this.searchinput;
+  route: string;
+  healthLabel = this.healthLabel;
+
+  constructor(
+    private recipeSearch: RecipesearchService,
+  ) { }
+
+  handleRecipeSearchClick = () => {
+    
+    this.recipeSearch.getRecipes(this.searchinput, this.healthLabel)
+      .subscribe(data => {
+       
+      this.recipes = data.hits.map(data => data.recipe);
+      console.log(this.recipes);
+      return this.recipes;
+    });
+   
+  }
 
   ngOnInit() {
   }
