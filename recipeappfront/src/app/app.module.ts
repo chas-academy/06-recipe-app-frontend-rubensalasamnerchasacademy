5ee457bb075a44d341b1e2ea9d605008fd4cd66c
Ruben Salas Amner <ruben.salas.amner@chasacademy.se>
2019-02-24 23:11:31 +0100

    add components login, navbar, profile and signup

--- recipeappfront/src/app/app.module.ts
+++ recipeappfront/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeslistComponent } from './recipeslist/recipeslist.component';
 import { RecipesdetailComponent } from './recipesdetail/recipesdetail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeslistComponent,
-    RecipesdetailComponent
+    RecipesdetailComponent,
+    NavbarComponent,
+    ProfileComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
