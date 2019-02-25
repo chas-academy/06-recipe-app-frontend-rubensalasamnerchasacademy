59d9b80ca6fb4e81a69d82796cbb49014100694b
Ruben Salas Amner <ruben.salas.amner@chasacademy.se>
2019-02-25 11:56:28 +0100

    add bootstrap and finalize imports

--- recipeappfront/src/app/app-routing.module.ts
+++ recipeappfront/src/app/app-routing.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
+import { recipeSearchService } from './recipesearch.service';
 import { RecipesdetailComponent } from './recipesdetail/recipesdetail.component';
 import { RecipeslistComponent } from './recipeslist/recipeslist.component';
+/* import { MyrecipesComponent } from './myrecipes/myrecipes.component'; */
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BeforeLoginService } from './Services/before-login.service';
+import { AfterLoginService } from './Services/after-login.service';
+
+
 
 const routes: Routes = [
-  {path: '', redirectTo: '/recipeslist', pathMatch: 'full'},
+  { path: '', redirectTo: '/recipeslist', pathMatch: 'full'},
   { path : 'recipeslist', component: RecipeslistComponent},
   { path : 'recipesdetail/:id', component: RecipesdetailComponent},
+  { path : 'login', component: LoginComponent, canActivate: [BeforeLoginService]},
+  { path : 'signup', component: SignupComponent, canActivate: [BeforeLoginService]},
+  { path : 'profile', component: ProfileComponent, canActivate: [AfterLoginService]},
 ]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- recipeappfront/src/app/app.module.ts
+++ recipeappfront/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeslistComponent } from './recipeslist/recipeslist.component';
 import { RecipesdetailComponent } from './recipesdetail/recipesdetail.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,13 +20,15 @@ import { SignupComponent } from './components/signup/signup.component';
     RecipeslistComponent,
     RecipesdetailComponent,
     NavbarComponent,
-    ProfileComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
